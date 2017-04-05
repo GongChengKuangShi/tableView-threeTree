56846fb930f773329ee6dd1165ce56248310965f
xiangronghua <xiangronghua@xiangronghuaMac.local>
2017-04-05 16:35:51 +0800

    first

---
+++ ThreeTreeTable--folding/ThreeTreeTable--folding/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThreeTreeTable--folding
+//
+//  Created by xiangronghua on 2017/4/5.
+//  Copyright © 2017年 xiangronghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
